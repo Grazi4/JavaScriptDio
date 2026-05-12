6556ddc8c08973d867bd7b458fac7ad1e8f512b2
Grazi Nunes <nunesgrazi99@gmail.com>
2026-05-12 09:11:05 -0300

    ultimo exercicio base

--- 07 - exercicios/exercicio3.js
+++ 07 - exercicios/exercicio3.js
@@ -11,3 +11,34 @@ Para calcular o percentual de imposto segue as aliquotas:
     De R$ 1100.01 a R$ 2500.00 = 10.00%
     Maior que 2500.00 = 15.00%
 */
+
+const {gets, print} = require('./funcoes-auxiliares-ex3')
+
+
+const salarioBruto = gets(); 
+const valorBeneficios = gets();
+
+function calcularPercentual(valor, percentual){
+    return (percentual / 100) * valor;
+}
+
+function pegarAliquota(salario){
+    
+    if (salario >= 0 && salario <= 1100){
+        return 5;
+    }
+    else if(salario >= 1100.01 && salario <= 2500) {
+        return 10;
+    } else {
+        return 15;
+    }
+}
+
+const aliquotaImposto = pegarAliquota(salarioBruto);
+
+const valorImposto = calcularPercentual(salarioBruto,aliquotaImposto);
+
+const valorATransferir = salarioBruto - valorImposto + valorBeneficios;
+
+print(valorATransferir);
+
